dc1b223927ef1bae416798db4c00e94489714557
Edward Langley <el-github@elangley.org>
2022-03-25 22:41:22 -0700

    feat(ci): add nix infrastructure

---
+++ nix/default.nix
@@ -0,0 +1,25 @@
+{ sources ? import ./sources.nix, pkgs ? import sources.nixpkgs {
+  overlays = [ ];
+  config = { };
+}, stdenv ? pkgs.stdenv, fetchurl ? pkgs.lib.fetchurl }:
+
+let
+  nodejs = pkgs.nodejs-16_x;
+  yarn = pkgs.yarn.overrideAttrs (_: { buildInputs = [ nodejs ]; });
+  hp = pkgs.haskellPackages.override {
+    overrides = self: super:
+      let
+        workaround140774 = hpkg:
+          with pkgs.haskell.lib;
+          overrideCabal hpkg (drv: { enableSeparateBinOutput = false; });
+      in { niv = workaround140774 super.niv; };
+  };
+
+in {
+  shell = stdenv.mkDerivation {
+    name = "shell-env";
+    buildInputs = [nodejs ];
+  };
+  niv = hp.niv;
+}
+
