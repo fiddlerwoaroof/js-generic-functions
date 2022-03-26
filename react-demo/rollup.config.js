bb33ef455f892a10cddeaee5515a5ff60ed6fb00
Edward Langley <el-github@elangley.org>
2022-03-26 00:10:02 -0700

    chore(deps): cleanup outdated deps

--- react-demo/rollup.config.js
+++ react-demo/rollup.config.js
@@ -1,22 +1,22 @@
-import babel from "rollup-plugin-babel";
-import npm_resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import replace from "rollup-plugin-replace";
-import livereload from "rollup-plugin-livereload";
+import { babel } from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
 
 export default {
   input: "main.js",
   output: {
     file: "dist/main.o.js",
-    browser: true,
     sourcemap: true,
     format: "iife",
   },
   plugins: [
-    //livereload({watch: 'dist'}),
-    replace({ "process.env.NODE_ENV": JSON.stringify("development") }),
-    babel({ exclude: "node_modules/**" }),
-    npm_resolve({ module: true, jsnext: true, main: true, browser: true }),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("development"),
+      preventAssignment: true,
+    }),
+    babel({ exclude: "node_modules/**", babelHelpers: "bundled" }),
+    nodeResolve({ browser: true }),
     commonjs(),
   ],
 };
